8c832e4b69502e1d066c785911c631cfcd95e2d7
Gur Telem <gtelem@apple.com>
2023-02-26 20:25:48 +0200

    Change formulas

--- yabai.rb
+++ yabai.rb
@@ -1,9 +1,9 @@
 class Yabai < Formula
   desc "A tiling window manager for macOS based on binary space partitioning."
-  homepage "https://github.com/koekeishiya/yabai"
-  url "https://github.com/koekeishiya/yabai/releases/download/v5.0.2/yabai-v5.0.2.tar.gz"
+  homepage "https://github.com/gur111/yabai"
+  url "https://github.com/gur111/yabai/releases/download/v5.0.2/yabai-v5.0.2.tar.gz"
   sha256 "981044e0339ba64b41ff9149d8e6fad4bfc0b7cc88df042754742f9e214bfe50"
-  head "https://github.com/koekeishiya/yabai.git"
+  head "https://github.com/gur111/yabai.git"
 
   depends_on :macos => :big_sur
 
@@ -35,7 +35,7 @@ class Yabai < Formula
     Build the configuration row by running:
       echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa"
 
-    README: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+    README: https://github.com/gur111/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
     EOS
   end
 
